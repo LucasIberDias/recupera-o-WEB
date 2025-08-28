cceb548b4b142b285b5f65bec91a3049253db3b9
LucasIberDias <Lucas.SKuggar@gmail.com>
2025-08-27 22:00:55 -0300

    recuperação realizada em sala de aula

---
+++ script.js
@@ -0,0 +1,46 @@
+let lista = [];
+function removerItem(index) {
+  lista.splice(index, 1);
+  atualizarLista();
+}
+let filtroAtual = "todos";
+
+function adicionarItem() {
+  const entradaItem = document.getElementById("entradaItem");
+  const entradaQuantidade = document.getElementById("entradaQuantidade");
+  const itemNome = entradaItem.value.trim();
+  const quantidade = parseInt(entradaQuantidade.value, 10);
+
+  if (itemNome && quantidade > 0) {
+    lista.push({ nome: itemNome, quantidade, comprado: false });
+    atualizarLista();
+  }
+}
+
+function atualizarLista() {
+  const listaCompras = document.getElementById("listaCompras");
+  listaCompras.innerHTML = "";
+
+  const itensFiltrados = lista.filter((item) => {
+    if (filtroAtual === "pendentes") return !item.comprado;
+    if (filtroAtual === "comprados") return item.comprado;
+    return true;
+  });
+
+  itensFiltrados.forEach((item, index) => {
+    const li = document.createElement("li");
+    li.className = `list-group-item d-flex justify-content-between align-items-center fade-in ${
+      item.comprado ? "comprado" : ""
+    }`;
+    const span = document.createElement("span");
+    span.innerHTML = `${item.nome} <span class="badge bg-primary rounded-pill">${item.quantidade}</span>`;
+    const div = document.createElement("div");
+    div.className = "btn-group";
+    div.innerHTML = `<button class="btn btn-sm btn-outline-success" onclick="marcarComprado(${index})">✔</button>
+        <button class="btn btn-sm btn-outline-danger" onclick="removerItem(${index})">✖</button>
+        `;
+    li.appendChild(span);
+    li.appendChild(div);
+    listaCompras.appendChild(li);
+  });
+}
